bd24645b8d2b6624c3b8e90b97ba97d7fb3ada7d
urkach <urkoinesbarcia@gmail.com>
2024-11-21 19:49:15 +0100

    Cambios en el juego: direccion, menu principal con la Q, etc.

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from tkinter import *
 from modo_obstaculos import gameLoop
 
+
+def obstaculos():
+    window.withdraw()
+    gameLoop(window)
+
+
 def modos_de_juego():
-    # Eliminar todos los widgets actuales
     for widget in window.winfo_children():
         widget.destroy()
 
-    # Cambiar el título y agregar nuevos botones y etiquetas
     window.title("Modos de Juego")
     nuevo_label = Label(window, text="Selecciona un Modo de Juego")
     nuevo_label.config(font=("Verdana", 24))
@@ -15,23 +19,20 @@ def modos_de_juego():
     boton_modo1 = Button(text="Modo Clásico", height=10, width=20)
     boton_modo1.place(relx=0.5, rely=0.5, anchor='center')
 
-    boton_modo2 = Button(text="Modo obstaculos", height=10, width=20, command=gameLoop)
+    boton_modo2 = Button(text="Modo obstaculos", height=10, width=20, command=obstaculos)
     boton_modo2.place(relx=0.5, rely=0.25, anchor='center')
 
     boton_multijugador = Button(text="MULTIJUGADOR", height=10, width=20)
     boton_multijugador.place(relx=0.5, rely=0.75, anchor='center')
 
-    # Botón "ATRÁS" en la esquina inferior derecha
     boton_atras = Button(text="ATRÁS", height=3, width=10, command=pantalla_inicio)
     boton_atras.place(relx=0.1, rely=0.9, anchor='se')
 
 
 def pantalla_inicio():
-    # Eliminar todos los widgets actuales
     for widget in window.winfo_children():
         widget.destroy()
 
-    # Configurar la pantalla inicial
     window.title("Juego de la serpiente")
     label = Label(window, text="¡Juego de la serpiente!")
     label.config(font=("Verdana", 24))
--- modo_obstaculos.py
+++ modo_obstaculos.py
@@ -1,33 +1,26 @@
 import pygame
 import random
 
-def gameLoop():
+
+def gameLoop(window):
     pygame.init()
 
-    # Colores
     WHITE = (255, 255, 255)
     BLACK = (0, 0, 0)
     GREEN = (0, 255, 0)
     RED = (213, 50, 80)
     YELLOW = (255, 255, 102)
 
-    # Dimensiones de la pantalla
     game_display = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
     surface = pygame.display.get_surface()
     SCREEN_WIDTH, SCREEN_HEIGHT = surface.get_width(), surface.get_height()
 
     pygame.display.set_caption('Snake Game')
 
-    # FPS (frames por segundo)
     clock = pygame.time.Clock()
-
-    # Tamaño de los bloques de la serpiente
     BLOCK_SIZE = 30
-
-    # Tamaño de los obstáculos
     OBSTACLE_SIZE = 40
 
-    # Fuentes
     font_style = pygame.font.SysFont("Cascadia Code PL, monospace", 25, bold=True)
     score_font = pygame.font.SysFont("Cascadia Code PL, monospace", 35, bold=True)
 
@@ -39,26 +32,22 @@ def gameLoop():
         for x in snake_list:
             pygame.draw.rect(game_display, GREEN, [x[0], x[1], block_size, block_size])
 
-    # Variables del juego
     game_over = False
     game_close = False
 
-    # Coordenadas de la serpiente
     x1 = SCREEN_WIDTH / 2
     y1 = SCREEN_HEIGHT / 2
 
-    # Movimiento de la serpiente
     x1_change = 0
     y1_change = 0
 
-    # Lista de la serpiente y su longitud inicial
     snake_length = 1
     snake_list = [[x1, y1]]
 
-    # Puntuación
     score = 0
 
-    # Posición de la fruta
+    direction = None  
+
     def generate_food():
         return (
             round(random.randrange(0, SCREEN_WIDTH - BLOCK_SIZE) / BLOCK_SIZE) * BLOCK_SIZE,
@@ -66,98 +55,88 @@ def gameLoop():
         )
 
     foodx, foody = generate_food()
-
-    # Obstáculos
     obstacles = []
 
-    # Bucle principal del juego
     while not game_over:
         while game_close:
             game_display.fill(BLACK)
-            message("Q - Salir || C - Jugar de nuevo", RED, SCREEN_WIDTH / 6, SCREEN_HEIGHT / 2)
+            message("¡HAS PERDIDO!", RED, SCREEN_HEIGHT / 1.25, SCREEN_HEIGHT / 5.5)
+            message(f"Tu puntuación ha sido: {score}", RED, SCREEN_WIDTH / 2.5, SCREEN_HEIGHT / 2.5)
+            message("Pulsa C para iniciar otra partida o Q para salir al menu principal.", RED, SCREEN_WIDTH / 4, SCREEN_HEIGHT / 1.5)
             pygame.display.update()
 
             for event in pygame.event.get():
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_q:
-                        game_over = True
-                        game_close = False
+                        pygame.quit()
+                        window.deiconify()  
+                        return
                     if event.key == pygame.K_c:
-                        gameLoop()
+                        gameLoop(window)
 
-        # Controlar los eventos de teclado
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 game_over = True
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
+    
+                if event.key == pygame.K_LEFT and direction != "RIGHT":
                     x1_change = -BLOCK_SIZE
                     y1_change = 0
-                elif event.key == pygame.K_RIGHT:
+                    direction = "LEFT"
+                elif event.key == pygame.K_RIGHT and direction != "LEFT":
                     x1_change = BLOCK_SIZE
                     y1_change = 0
-                elif event.key == pygame.K_UP:
+                    direction = "RIGHT"
+                elif event.key == pygame.K_UP and direction != "DOWN":
                     y1_change = -BLOCK_SIZE
                     x1_change = 0
-                elif event.key == pygame.K_DOWN:
+                    direction = "UP"
+                elif event.key == pygame.K_DOWN and direction != "UP":
                     y1_change = BLOCK_SIZE
                     x1_change = 0
+                    direction = "DOWN"
 
-        # Verificar si la serpiente ha chocado con los límites de la pantalla
         if x1 >= SCREEN_WIDTH or x1 < 0 or y1 >= SCREEN_HEIGHT or y1 < 0:
             game_close = True
 
-        # Movimiento de la serpiente
         x1 += x1_change
         y1 += y1_change
         game_display.fill(BLACK)
 
-        # Dibujar los obstáculos
         for obstacle in obstacles:
             pygame.draw.rect(game_display, RED, [obstacle[0], obstacle[1], OBSTACLE_SIZE, OBSTACLE_SIZE])
-            # Verificar colisión con obstáculos
             if obstacle[0] <= x1 < obstacle[0] + OBSTACLE_SIZE and obstacle[1] <= y1 < obstacle[1] + OBSTACLE_SIZE:
                 game_close = True
 
-        # Dibujar la fruta
         pygame.draw.rect(game_display, YELLOW, [foodx, foody, BLOCK_SIZE, BLOCK_SIZE])
 
-        # Actualizar la lista de la serpiente
         snake_head = [x1, y1]
         snake_list.append(snake_head)
 
         if len(snake_list) > snake_length:
             del snake_list[0]
 
-        # Comprobar colisión con el propio cuerpo
         for segment in snake_list[:-1]:
             if segment == snake_head:
                 game_close = True
 
         draw_snake(BLOCK_SIZE, snake_list)
-        # Verificar si la serpiente ha comido la fruta
         if x1 - BLOCK_SIZE <= foodx <= x1 + BLOCK_SIZE and y1 - BLOCK_SIZE <= foody <= y1 + BLOCK_SIZE:
             foodx, foody = generate_food()
             snake_length += 1
-            score += 10
+            score += 1
 
-            # Añadir un nuevo obstáculo
             new_obstacle = [
                 round(random.randrange(0, SCREEN_WIDTH - OBSTACLE_SIZE) / BLOCK_SIZE) * BLOCK_SIZE,
                 round(random.randrange(0, SCREEN_HEIGHT - OBSTACLE_SIZE) / BLOCK_SIZE) * BLOCK_SIZE,
             ]
             obstacles.append(new_obstacle)
 
-        # Dibujar la puntuación
         score_text = score_font.render("Puntuación: " + str(score), True, WHITE)
         game_display.blit(score_text, [10, 10])
         pygame.display.update()
 
-        # Controlar la velocidad del juego
         clock.tick(15)
 
     pygame.quit()
     quit()
-
-if __name__ == '__main__':
-    gameLoop()
